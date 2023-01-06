a17647df1b23ba452efdf3a95e7c9dab0f3b77ac
Matthew <mbrooks9622@gmail.com>
2023-01-06 15:59:49 +0000

    added function and added prompt message for user

--- starter/script.js
+++ starter/script.js
@@ -1,3 +1,18 @@
+// * Generate a password when the button is clicked
+//   * Present a series of prompts for password criteria
+//     * Length of password
+//       * At least 10 characters but no more than 64.
+//     * Character types
+//       * Lowercase
+//       * Uppercase
+//       * Numeric
+//       * Special characters ($@%&*, etc)
+//   * Code should validate for each input and at least one character type should be selected
+//   * Once prompts are answered then the password should be generated and displayed in an alert or written to the page
+
+//NOTE TO SELF - Go back to watch the beginning of Friday 06th Jan 2023 video to help with the Home Work Challenge
+
+
 // Array of special characters to be included in password
 var specialCharacters = [
   '@',
@@ -90,7 +105,38 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  let numberCharacters = window.prompt("how many characters")
+  console.log(numberCharacters)
+  let confirmSpecial = window.confirm("Do You Want Special Characters")
+  console.log("confirmSpecial", confirmSpecial);
+  
+  let numericCharacters = window.prompt("Do you want numeric characters")
+  console.log("numericCharacters", numericCharcters);
 
+  let lowerCasedCharacters = window.prompt("Do you want lower case characters")
+  console.log("lowerCasedCharacters", lowerCasedCharacters);
+
+  let upperCasedCharacters = window.confirm("Do You Want upper Cased Characters")
+  console.log("upperCasedCharacters", upperCasedCharacters);
+//above I have inputed a series of prompts for password criteria
+}
+
+//Put if statements here and else if
+if (numberCharacters) {
+  //think of how to write it here
+  
+}
+
+else if (condition) {
+  
+}
+
+if (condition) {
+  
+}
+
+else if (condition) {
+  
 }
 
 // Function for getting a random element from an array
@@ -100,8 +146,17 @@ function getRandom(arr) {
 
 // Function to generate password with user input
 function generatePassword() {
-
+  //function not defined so I am going to define it here and inform the user when they click the button in the console log that the button has been clicked.
+  console.log("Button clicked")
+  getPasswordOptions();
+//I have changed the undefined display message to return a new message of "Your Random Password will be Generated" when the user clicks the Generate Password button.
+  return "Your Random Password Will Be Generated";
 }
+
+Math.random()
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
