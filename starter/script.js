39e2049b64127c6fb67b9865a71c8a544381e157
Matthew <mbrooks9622@gmail.com>
2023-01-08 12:52:12 +0000

    randon password function operational

--- starter/script.js
+++ starter/script.js
@@ -37,7 +37,7 @@
 
 
 // Array of special characters to be included in password
-var specialCharacters = [
+var special = [
   '@',
   '%',
   '+',
@@ -64,10 +64,10 @@ var specialCharacters = [
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
+var lower = [
   'a',
   'b',
   'c',
@@ -97,7 +97,7 @@ var lowerCasedCharacters = [
 ];
 
 // Array of uppercase characters to be included in password
-var upperCasedCharacters = [
+var upper = [
   'A',
   'B',
   'C',
@@ -128,52 +128,99 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-  let numberCharacters = window.prompt("how many characters")
+  let numberCharacters = parseInt(window.prompt("how many characters"))
   console.log(numberCharacters)
+while (isNaN(numberCharacters) || numberCharacters  < 10 || numberCharacters > 64) {
+ numberCharacters = parseInt(window.prompt("how many characters"))
+}
+
   let confirmSpecial = window.confirm("Do You Want Special Characters")
   console.log("confirmSpecial", confirmSpecial);
-  
-  let numericCharacters = window.prompt("Do you want numeric characters")
-  console.log("numericCharacters", numericCharcters);
 
-  let lowerCasedCharacters = window.prompt("Do you want lower case characters")
+  
+  let numericCharacters = window.confirm("Do you want numeric characters")
+  console.log("numericCharacters", numericCharacters);
+
+  let lowerCasedCharacters = window.confirm("Do you want lower case characters")
   console.log("lowerCasedCharacters", lowerCasedCharacters);
 
   let upperCasedCharacters = window.confirm("Do You Want upper Cased Characters")
   console.log("upperCasedCharacters", upperCasedCharacters);
 //above I have inputed a series of prompts for password criteria
+return{
+  numberCharacters, confirmSpecial, numericCharacters, lowerCasedCharacters, upperCasedCharacters
 }
+
+}
+
+function random(array){
+  return array[Math.floor]
+}
+
 
 //Put if statements here and else if
-if (numberCharacters) {
-  //think of how to write it here
+// if (numberCharacters) {
+//   //think of how to write it here
   
-}
+// }
 
-else if (condition) {
+// else if (condition) {
   
-}
+// }
 
-if (condition) {
+// if (condition) {
   
-}
+// }
 
-else if (condition) {
+// else if (condition) {
   
-}
+// }
 
 // Function for getting a random element from an array
-function getRandom(arr) {
-
+function getRandom(array) {
+return array[Math.floor(Math.random()*array.length)]
 }
 
 // Function to generate password with user input
 function generatePassword() {
   //function not defined so I am going to define it here and inform the user when they click the button in the console log that the button has been clicked.
   console.log("Button clicked")
-  getPasswordOptions();
+  const {
+    numberCharacters, confirmSpecial, numericCharacters, lowerCasedCharacters, upperCasedCharacters
+  } = getPasswordOptions();
+
+const password = []
+
+let possibleOptions = []
+
+if (confirmSpecial) {
+  possibleOptions = possibleOptions.concat(special)
+  password.push(getRandom(special))
+}
+
+if (numericCharacters) {
+  possibleOptions = possibleOptions.concat(numbers)
+  password.push(getRandom(numbers))
+}
+
+if (lowerCasedCharacters) {
+  possibleOptions = possibleOptions.concat(lower)
+  password.push(getRandom(lower))
+}
+
+
+if (upperCasedCharacters) {
+  possibleOptions = possibleOptions.concat(upper)
+  password.push(getRandom(upper))
+}
+
+for (let i = password.length; i < numberCharacters; i++) {
+ password.push(getRandom(possibleOptions))
+}
+console.log(password.join(""))
+
 //I have changed the undefined display message to return a new message of "Your Random Password will be Generated" when the user clicks the Generate Password button.
-  return "Your Random Password Will Be Generated";
+  return password.join("")
 }
 
 Math.random()
